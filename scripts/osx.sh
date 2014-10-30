e65a8671101855972bdfa2d2c1ca0efd61bd22d2
Gerardo Rochín <gerardorochin@gmail.com>
2014-10-29 21:36:13 -0700

    terminal and iterm2 support removed

--- scripts/osx.sh
+++ scripts/osx.sh
@@ -168,26 +168,6 @@ os=`uname`
 
 
     ###############################################################################
-    # Terminal & iTerm 2                                                          #
-    ###############################################################################
-
-    # Only use UTF-8 in Terminal.app
-    defaults write com.apple.terminal StringEncodings -array 4
-
-    # Use a modified version of the Solarized Dark theme by default in Terminal.app
-    TERM_PROFILE='Solarized Dark xterm-256color';
-    CURRENT_PROFILE="$(defaults read com.apple.terminal 'Default Window Settings')";
-    if [ "${CURRENT_PROFILE}" != "${TERM_PROFILE}" ]; then
-        open "${HOME}/init/${TERM_PROFILE}.terminal";
-        sleep 1; # Wait a bit to make sure the theme is loaded
-        defaults write com.apple.terminal 'Default Window Settings' -string "${TERM_PROFILE}";
-        defaults write com.apple.terminal 'Startup Window Settings' -string "${TERM_PROFILE}";
-    fi;
-
-    # Install the Solarized Dark theme for iTerm
-    open "${HOME}/init/Solarized Dark.itermcolors"
-
-    ###############################################################################
     # Time Machine                                                                #
     ###############################################################################
 
@@ -219,8 +199,7 @@ os=`uname`
     ###############################################################################
 
     for app in "Activity Monitor" \
-        "Dock" "Finder" "SystemUIServer" \
-        "Terminal"; do
+        "Dock" "Finder" "SystemUIServer"; do 
         killall "${app}" > /dev/null 2>&1
     done
 
