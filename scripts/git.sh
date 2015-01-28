d63db3567875c0f7ddaae3c378ab92e882225ba1
Gerardo Rochín <gerardorochin@gmail.com>
2015-01-28 11:16:48 -0700

    custom colors

--- scripts/git.sh
+++ scripts/git.sh
@@ -17,7 +17,16 @@ then
     # Git settings
     git config --global user.name "Gerardo Rochín"
     git config --global user.email gerardorochin@gmail.com
+
     git config --global color.ui true
+    git config --global color.diff.meta "yellow reverse bold"
+
+    git config --global color.status true
+    git config --global color.status.branch "green"
+    git config --global color.status.added "green"
+    git config --global color.status.changed "red"
+    git config --global color.status.untracked "yellow"
+
     git config --global core.editor "vim"
 
     echo -e "Done\n"
